f1097ddf155032bc48e2ef74bdf9ad373f4db1ca
Brian Morearty <brian@morearty.org>
2025-03-23 15:01:20 -0700

    Unit 1 lesson 2

--- greek.js
+++ greek.js
@@ -8,6 +8,18 @@ export const allPhrases = {
         { foreign: 'Καλησπέρα! (Kalispera!)', english: 'Good afternoon! (Used after 1pm.)' },
         { foreign: 'Γεια! (Ya!)', english: 'Hi! or Bye! (informal)' },
       ],
+      lesson2: [
+        {
+          foreign: 'Τι κάνετε; (tee KAHnehteh)',
+          english: 'How are you? (Literally, what do you do?) (formal)',
+        },
+        { foreign: 'Τι κάνεις; (tee KAHnees)', english: 'How are you? (informal)' },
+        { foreign: 'τι', english: 'what' },
+        { foreign: 'κάνω (KAHnoh)', english: 'I do' },
+        { foreign: 'κάνετε (KAHnehteh)', english: 'you do (formal)' },
+        { foreign: 'κάνεις (KAHnees)', english: 'you do (informal)' },
+        { foreign: 'Τι γίνεται; (tee YEEnehteh)', english: "What's up?" },
+      ],
     },
   },
 };
